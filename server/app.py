fbfd81d97791d8f08d31abdec0b3320ca15638a1
ChaseTeague <teaguechase@yahoo.com>
2024-07-04 20:58:54 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -79,6 +79,30 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self,id):
+        newsletter = Newsletter.query.filter(Newsletter.id == id).first()
+
+        for attr in request.form:
+            setattr(newsletter, attr, request.form[attr])
+
+        db.session.add(newsletter)
+        db.session.commit()
+
+        response_dict = newsletter.to_dict()
+        return make_response(response_dict, 200)
+    
+    def delete(self,id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_body = {
+            "message": "Record deleted successfully"
+        }
+        return make_response(response_body, 200)
+
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
